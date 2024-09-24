4514ed5c1b0c68238d64bae42ca1531d062c7a64
Abraham Vineel Katikala <abrahamvineel@gmail.com>
2024-09-23 20:50:10 -0400

    kv store init

---
+++ main.go
@@ -0,0 +1,13 @@
+/*Stage 1: Build a basic in-memory key-value store with simple CRUD operations.
+Stage 2: Add persistence to disk (e.g., using a write-ahead log or appending to a file).
+Stage 3: Add concurrency using goroutines, locks, and channels.
+Stage 4: Implement an LSM Tree-based or B-Tree-based on-disk storage model.
+Stage 5: Scale out by implementing sharding or replication across nodes. */
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello")
+}

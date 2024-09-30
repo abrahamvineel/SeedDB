3f3b45a1aadfa03b5a0e944f332e4bb98c4bea5c
Abraham Vineel Katikala <abrahamvineel@gmail.com>
2024-09-30 19:53:58 -0400

    wal features

---
+++ wal.go
@@ -0,0 +1,13 @@
+//implement wal
+
+// Essential Features:
+// Durability
+// Atomicity
+// Sequential Logging
+// Crash Recovery
+// Checkpointing
+// Efficient Log Truncation
+// Log Synchronization
+// Concurrency and Thread Safety
+// Optional (Advanced) Features: 9. Log Compaction 10. Fault Tolerance 11. Batching Writes 12. Versioning or Log Metadata 13. Support for Distributed Systems
+

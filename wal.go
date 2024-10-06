a2d107759a383c0e649c8e89f512da9efde2a91e
Abraham Vineel Katikala <abrahamvineel@gmail.com>
2024-10-05 20:59:26 -0400

    init wal

--- wal.go
+++ wal.go
@@ -10,4 +10,8 @@
 // Log Synchronization
 // Concurrency and Thread Safety
 // Optional (Advanced) Features: 9. Log Compaction 10. Fault Tolerance 11. Batching Writes 12. Versioning or Log Metadata 13. Support for Distributed Systems
+package main
 
+struct wal {
+
+}

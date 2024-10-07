81fb247e38f5c363311dbeebe3890861000c94b1
Abraham Vineel Katikala <abrahamvineel@gmail.com>
2024-10-06 20:42:46 -0400

    wal and log record init

--- wal.go
+++ wal.go
@@ -12,6 +12,21 @@
 // Optional (Advanced) Features: 9. Log Compaction 10. Fault Tolerance 11. Batching Writes 12. Versioning or Log Metadata 13. Support for Distributed Systems
 package main
 
-struct wal {
+import "os"
+
+type LogRecord struct {
+	LogSequenceNumber uint64
+	Data              []byte
+	CRC               uint32
+}
+
+type WAL struct {
+	file                  *os.File
+	LastLogSequenceNumber uint64
+}
+
+func createWAL(filePath string) (*WAL, error) {
 
 }
+
+func createLogEntry() {}

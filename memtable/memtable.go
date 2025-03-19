79d89175a0d6531cada3d2b554c13b25c8d60c97
Abraham Vineel Katikala <abrahamvineel@gmail.com>
2025-03-18 20:22:43 -0400

    skip list search

--- memtable/memtable.go
+++ memtable/memtable.go
@@ -2,7 +2,25 @@
 package memtable
 
 type SkipList struct {
-	Value uint64
+	Key   string
+	Value string
 	Down  *SkipList
 	Left  *SkipList
+	Right *SkipList
+}
+
+func (skipList *SkipList) search(key string) string {
+	for skipList.Right != nil {
+		if skipList.Key == key {
+			return skipList.Value
+		}
+		if skipList.Key < key {
+			skipList = skipList.Right
+		} else if skipList.Key > key {
+			skipList = skipList.Left
+		} else {
+			skipList = skipList.Down
+		}
+	}
+	return ""
 }
